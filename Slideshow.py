2930685379ec648d3248d711cf61fbe34a1177d0
Kevin Kim <chaeya@gmail.com>
2024-08-03 00:53:08 +0900

    Update from latest upstream

--- Slideshow.py
+++ Slideshow.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 import gi
 import os
-gi.require_version('WebKit2', '4.0')
+
+# Adjust the version to match the available version on your system
+gi.require_version('WebKit2', '4.1')
 gi.require_version('Gtk', '3.0')
 gi.require_version('Gdk', '3.0')
+
 from gi.repository import GLib, Gdk, Gtk, WebKit2
 from configparser import ConfigParser
 import subprocess
-
 import sys
 import locale
 from optparse import OptionParser
@@ -17,19 +19,20 @@ class SlideshowViewer(WebKit2.WebView):
         self.path = path
 
         config = ConfigParser()
-        config.read(os.path.join(self.path,'slideshow.conf'))
+        config.read(os.path.join(self.path, 'slideshow.conf'))
 
         slideshow_main = 'file://' + os.path.join(self.path, 'slides', 'index.html')
 
         parameters = []
-        # slideshow_locale = self._find_available_locale(locale)
-        slideshow_locale = self._find_available_locale("ko")
+        slideshow_locale = self._find_available_locale(locale)
         parameters.append('locale=%s' % slideshow_locale)
         if rtl:
             parameters.append('rtl')
         if controls:
             parameters.append('controls')
 
+        context = WebKit2.WebContext.get_default()
+        context.set_cache_model(WebKit2.CacheModel.DOCUMENT_VIEWER)
         WebKit2.WebView.__init__(self)
         parameters_encoded = '&'.join(parameters)
         url = '%s#%s' % (slideshow_main, parameters_encoded)
@@ -37,15 +40,29 @@ class SlideshowViewer(WebKit2.WebView):
         print(url)
 
         settings = self.get_settings()
-        settings.set_enable_write_console_messages_to_stdout(True)
-        settings.set_allow_file_access_from_file_urls(True)
-        self.set_settings(settings)
+        settings.set_property('allow-file-access-from-file-urls', True)
 
-        config_width = int(config.get('Slideshow','width'))
-        config_height = int(config.get('Slideshow','height'))
+        config_width = int(config.get('Slideshow', 'width'))
+        config_height = int(config.get('Slideshow', 'height'))
         self.set_size_request(config_width, config_height)
 
-        self.connect('decide-policy', self._on_decide_policy)
+        self.connect('context-menu', self.on_context_menu)
+        self.connect('decide-policy', self.on_slideshow_link_clicked)
+
+    def on_context_menu(self, unused_web_view, unused_context_menu,
+                        unused_event, unused_hit_test_result):
+        return True
+
+    def on_slideshow_link_clicked(self, web_view, decision, decision_type):
+        gi.require_version('WebKit2', '4.1')
+        from gi.repository import WebKit2
+        if decision_type == WebKit2.PolicyDecisionType.NEW_WINDOW_ACTION:
+            request = decision.get_request()
+            uri = request.get_uri()
+            decision.ignore()
+            misc.launch_uri(uri)
+            return True
+        return False
 
     def _find_available_locale(self, locale):
         base_slides_dir = os.path.join(self.path, 'slides', 'l10n')
@@ -61,40 +78,52 @@ class SlideshowViewer(WebKit2.WebView):
                     return test_locale
         return 'C'
 
-    def _on_decide_policy(self, view, decision, decision_type):
-        if decision_type == WebKit2.PolicyDecisionType.NAVIGATION_ACTION:
-            navigation_action = decision.get_navigation_action()
-            request = navigation_action.get_request()
-            uri = request.get_uri()
-            decision.use()
+def progress_increment(progressbar, fraction):
+    new_fraction = progressbar.get_fraction() + fraction
+    if new_fraction > 1:
+        progressbar.set_fraction(1.0)
         return False
 
-if __name__ == '__main__':
-    # Gdk.threads_init()
+    progressbar.set_fraction(new_fraction)
+    return True
 
-    default_path = os.path.join( os.path.abspath(os.path.dirname(sys.argv[0])) , 'build', 'ubuntu' )
-    default_locale = locale.getlocale()[0]
-    
-    parser = OptionParser(usage="usage: %prog [options] [slideshow]")
-    parser.add_option("-l", "--locale", help="LOCALE to use for the slideshow", metavar="LOCALE", default=default_locale)
-    parser.add_option("-r", "--rtl", action="store_true", help="use output in right-to-left format")
-    parser.add_option("-c", "--controls", action="store_true", help="Enable controls in the slideshow (you may need to resize the window)")
-    parser.add_option("-p", "--path", help="path to the SLIDESHOW which will be presented", metavar="SLIDESHOW", default=default_path)
+default_path = os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), 'build', 'ubuntu')
+default_locale = locale.getlocale()[0]
 
-    (options, args) = parser.parse_args()
-    options.path = os.path.abspath(options.path)
+parser = OptionParser(usage="usage: %prog [options] [slideshow]")
+parser.add_option("-l", "--locale", help="LOCALE to use for the slideshow", metavar="LOCALE", default=default_locale)
+parser.add_option("-r", "--rtl", action="store_true", help="use output in right-to-left format")
+parser.add_option("-c", "--controls", action="store_true", help="Enable controls in the slideshow (you may need to resize the window)")
+parser.add_option("-p", "--path", help="path to the SLIDESHOW which will be presented", metavar="SLIDESHOW", default=default_path)
 
-    slideshow_window = Gtk.Window()
-    slideshow_window.set_title("Ubiquity Slideshow")
-    slideshow_window.connect('destroy',Gtk.main_quit)
-    slideshow_window.set_resizable(False)
+(options, args) = parser.parse_args()
+options.path = os.path.abspath(options.path)
+if not os.path.exists(options.path):
+    print("\033[91m * Please build the slideshow content first by using the make command * \033[0m")
+    sys.exit()
 
-    slideshow_container = Gtk.VBox()
-    slideshow_window.add(slideshow_container)
+slideshow_window = Gtk.Window()
+slideshow_window.set_title("Ubiquity Slideshow with Webkit")
+slideshow_window.connect('destroy', Gtk.main_quit)
+slideshow_window.set_resizable(False)
 
-    slideshow = SlideshowViewer(options.path, locale=options.locale, rtl=options.rtl, controls=options.controls)
+slideshow_container = Gtk.VBox()
+slideshow_window.add(slideshow_container)
 
-    slideshow_container.add(slideshow)
-    slideshow_window.show_all()
+slideshow = SlideshowViewer(options.path, locale=options.locale, rtl=options.rtl, controls=options.controls)
 
-    Gtk.main()
+install_progressbar = Gtk.ProgressBar()
+install_progressbar.set_margin_top(8)
+install_progressbar.set_margin_end(8)
+install_progressbar.set_margin_bottom(8)
+install_progressbar.set_margin_start(8)
+install_progressbar.set_fraction(0)
+
+slideshow_container.add(slideshow)
+slideshow_container.add(install_progressbar)
+
+slideshow_container.set_child_packing(install_progressbar, True, False, 0, 0)
+
+slideshow_window.show_all()
+
+Gtk.main()
